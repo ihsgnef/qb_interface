5b0e0e8f8d0346cdf25b26249a95439a21720816
Ubuntu <ubuntu@ip-172-31-26-71.us-west-2.compute.internal>
2017-11-16 04:46:26 +0000

    update

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -92,7 +92,7 @@ function setsource(url, keys, output) {
     });
 }
 
-setsource("http://localhost:80/answers.json");
+setsource("http://qbinterface.club/answers.json");
 
 var voice_msg = new SpeechSynthesisUtterance();
 var voices = window.speechSynthesis.getVoices();
