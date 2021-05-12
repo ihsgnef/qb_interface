9ebf24627622cbae756f6f6e0561393be5165af6
ihsgnef <shifeng@cs.umd.edu>
2021-05-12 13:34:56 +0000

    up

---
+++ scripts/download.py
@@ -0,0 +1,6 @@
+from augment.utils import shell
+
+s3_dir = 's3://pinafore-us-west-2/public/guesser-centaur.zip'
+data_dir = 'data/guesser.zip'
+shell(f'aws s3 cp {s3_dir} {data_dir}')
+shell(f'unzip {data_dir} -d data/')
