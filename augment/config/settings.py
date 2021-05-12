d26f714f769c6e1024a1aff24e7713f2491dcec0
sjtufs <sjtufs@gmail.com>
2021-05-12 09:35:11 -0400

    update

--- augment/config/settings.py
+++ augment/config/settings.py
@@ -1,6 +1,6 @@
-CODE_DIR = '~/qb_interface'
+CODE_DIR = '/fs/clip-quiz/shifeng/centaur'
 DATA_DIR = f'{CODE_DIR}/data'
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://ubuntu@localhost:5433/augment'
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://shifeng@localhost:5433/augment'
 USE_MULTIPROCESSING = True
 USER_STATS_CACHE = True
 MP_CONTEXT = 'fork'
